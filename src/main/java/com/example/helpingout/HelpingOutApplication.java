828e4bd750167de7cadea0c29f4ab98b4eabb614
jdargatz <julie.dargatz@gmail.com>
2021-08-17 19:08:49 -0500

    Login feature working with hard-coded values

---
+++ src/main/java/com/example/helpingout/HelpingOutApplication.java
@@ -0,0 +1,13 @@
+package com.example.helpingout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelpingOutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelpingOutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/helpingout/HelpingOutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.helpingout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpingOutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
