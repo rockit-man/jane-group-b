4656918237ee671de2230f3a3753afd522e3d441
jdargatz <julie.dargatz@gmail.com>
2021-08-22 10:02:43 -0500

    Linked registration page to home page and added some bootstrap.

--- src/main/java/com/example/helpingout/controllers/MvcConfig.java
+++ src/main/java/com/example/helpingout/controllers/MvcConfig.java
@@ -7,12 +7,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class MvcConfig implements WebMvcConfigurer {
 
-    // Will need to add to every URI/view pairs, correct?
+    // Add each HTML page in here for view
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/index").setViewName("index");
         registry.addViewController("/").setViewName("index");
         registry.addViewController("/user-home").setViewName("user-home");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/registration").setViewName("registration");
     }
 
 }
