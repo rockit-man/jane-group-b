a446b6df4ce6776d467fdf5426c1f979a5415606
JFarm0 <79594115+JFarm0@users.noreply.github.com>
2021-09-12 21:14:17 -0500

    Company MVC built

--- src/main/java/com/example/helpingout/models/dto/CompanyTagDTO.java
+++ src/main/java/com/example/helpingout/models/dto/CompanyTagDTO.java
@@ -1,4 +1,33 @@
 package com.example.helpingout.models.dto;
 
+import com.example.helpingout.models.Company;
+import com.example.helpingout.models.Tag;
+
+import javax.validation.constraints.NotNull;
+
 public class CompanyTagDTO {
+
+    @NotNull
+    private Company company;
+
+    @NotNull
+    private Tag tag;
+
+    public CompanyTagDTO() {}
+
+    public Company getCompany() {
+        return company;
+    }
+
+    public void setCompany(Company company) {
+        this.company = company;
+    }
+
+    public Tag getTag() {
+        return tag;
+    }
+
+    public void setTag(Tag tag) {
+        this.tag = tag;
+    }
 }
--- src/main/java/com/example/helpingout/repositories/CompanyRepository.java
+++ src/main/java/com/example/helpingout/repositories/CompanyRepository.java
@@ -1,4 +1,9 @@
 package com.example.helpingout.repositories;
 
-public class CompanyRepository {
+import com.example.helpingout.models.Company;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CompanyRepository extends CrudRepository<Company, Integer> {
 }
